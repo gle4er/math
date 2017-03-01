7c6516314bb051832e4fa0f6fe984cefffec7230
Гриша Тарасенко <fle4er@yandex.ru>
2017-03-01 08:57:13 +0700

    мпи, метод зойделя

--- lab2/main.c
+++ lab2/main.c
@@ -80,30 +80,30 @@ void load() //загрузка матрицы с файла
             fscanf(file, "%lf", &matrix[i][j]);
         }
     answer = (double*) malloc(sizeof(double) * cnt);
+    for (int i = 0; i < cnt; i++)
+        answer[i] = 0;
     answer_tmp = (double*) malloc(sizeof(double) * cnt);
     return;
 }
 
-int calculating()
+int calculating() //кулькулируем кол-во шагов
 {
     double max = 0, tmp, N = 0;
     for (int i = 0; i < cnt; i++) {
         tmp = 0;
         for (int j = 0; j < cnt; j++) {
             if (matrix[i][j] < 0)
-                matrix[i][j] *= -1;
-            tmp += matrix[i][j];
+                tmp -= matrix[i][j];
+            else
+                tmp += matrix[i][j];
         }
         if (tmp > max)
             max = tmp;
     }
     tmp = 0;
-    for (int i = 0; i < cnt; i++) {
-        if (matrix[i][3] < 0)
-            matrix[i][3] *= -1;
-        if (matrix[i][3] > tmp)
-            tmp = matrix[i][3];
-    }
+    for (int i = 0; i < cnt; i++) 
+        if (matrix[i][cnt] > tmp || (matrix[i][cnt] * -1) > tmp)
+            tmp = matrix[i][cnt];
     N = (double) EPSYL * (1 - max) / tmp;
     N = log(N);
     N /= log(max);
@@ -122,7 +122,66 @@ void mpi()
         }
     }
     int N = calculating();
-    printf("%d waroB\n", N);
+    printm();
+    printf("%d\n", N);
+    double prev = 0, max = 0;
+    for (int i = 0; i < N; i++) {
+        prev = max;
+        printf("Step %d: ", i);
+        for (int j = 0; j < cnt; j++) { 
+            double tmp = 0;
+            for (int k = 0; k < cnt; k++) 
+                tmp += matrix[j][k] * answer[k];
+            answer_tmp[j] = tmp;
+        }
+        for (int j = 0; j < cnt; j++) {
+            if (answer[j] > max || answer[j] * -1 > max)
+                prev = answer[j];
+            answer[j] = matrix[j][cnt] - answer_tmp[j];
+            printf("%.2lf\t", answer[j]);
+        }
+        printf("\n");
+        double e = EPSYL;
+        if (!((max - prev) < e))
+            break;
+    }
+}
+
+void mpi_mod()
+{
+    /*for (int i = 0; i < cnt; i++) {
+        double koeff = matrix[i][i];
+        for (int j = 0; j < cnt + 1; j++) {
+            if (i == j)
+                matrix[i][i] = 0;
+            matrix[i][j] /= koeff;
+        }
+    }*/
+    int N = calculating();
+    printm();
+    printf("%d\n", N);
+    double prev = 0, max = 0;
+    for (int i = 0; i < 13; i++) {
+        prev = max;
+        printf("Step %d: ", i);
+        for (int j = 0; j < cnt; j++) 
+            answer_tmp[j] = answer[j];
+        for (int j = 0; j < cnt; j++) {
+            double tmp = 0;
+            for (int k = 0; k < j; k++)
+                tmp += matrix[j][k] * answer[k];
+            for (int k = j + 1; k < cnt; k++)
+                tmp += matrix[j][k] * answer_tmp[k];
+            if (answer[j] > max || answer[j] * -1 > max)
+                prev = answer[j];
+            answer[j] = (matrix[j][cnt] - tmp) / matrix[j][j];
+            printf("%.2lf\t", answer[j]);
+        }
+        printf("\n");
+        double e = EPSYL;
+        if (!((max - prev) < e))
+            break;
+    }
 }
 
 int main()
@@ -134,5 +193,8 @@ int main()
     for (int i = 0; i < cnt; i++)
         printf("x%d = %.1lf; ", i + 1, answer[i]);
     printf("\n");
+    free(answer);
+    free(answer_tmp);
+    free(matrix);
     return 0;
 }
