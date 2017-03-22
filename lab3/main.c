02cdcf79a145a664324ffbe5611464d59c150db8
Гриша Тарасенко <fle4er@yandex.ru>
2017-03-22 11:01:16 +0700

    Пофиксел

--- lab3/main.c
+++ lab3/main.c
@@ -2,9 +2,9 @@
 #include <stdlib.h>
 #include <math.h>
 
-#define MPD
+//#define MPD
 //#define MX
-//#define MN
+#define MN
 
 double a, b, c = 0;
 double e = 0.001;
@@ -12,31 +12,33 @@ double intrvl = 0.5;
 
 double f(double x)
 {
-    return x * x + 2 * x - 3;
+    return x * x - 1;
 }
 
 double f1(double x)
 {
-    return 2 * x + 2;
+    return 2 * x;
 }
 
 void mpd()
 {
-    while (fabs(b - a) > e || f(c) != 0) {
+    int i;
+    for (i = 0; fabs(b - a) > e && f(c) != 0; i++) {
         c = (a + b) / 2;
-        if (f(c) == 0)
-            break;
         if (f(a) * f(c) < 0)
             b = c;
         else
             a = c;
     }
+    printf("steps: %d\n", i);
 }
 
 void mx()
 {
     double tmp = 0;
-    while (fabs(c - tmp) > e || f(c) != 0) {
+    int i, flg = 0;
+    for (i = 0; (fabs(c - tmp) > e || !flg) && f(c) != 0; i++) {
+        flg++;
         tmp = c;
         c = a - f(a) * (b - a) / (f(b) - f(a));
         if (f(a) * f(c) < 0)
@@ -44,19 +46,19 @@ void mx()
         else
             a = c;
     } 
+    printf("steps: %d\n", i);
 }
 
 void mn()
 {
     double tmp = a;
-    for (double i = a; i <= b; i += intrvl)
-        if (fabs(f(tmp)) > fabs(f(i)))
-            tmp = i;
+    int j;
     c = (-1) * (f(tmp) / f1(tmp)) + tmp;
-    while (f(c) != 0 || fabs(c - tmp) > e) {
+    for (j = 0; f(c) != 0 && fabs(c - tmp) > e; j++) {
         tmp = c;
         c = tmp - (f(tmp) / f1(tmp));
     } 
+    printf("steps: %d\n", j);
 }
 
 int main(int argc, char **argv)
