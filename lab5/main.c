0459befc327ce742d6b4b9389facf179d7303f91
Гриша Тарасенко <fle4er@yandex.ru>
2017-03-26 21:08:33 +0700

    добавил просьбы

--- lab5/main.c
+++ lab5/main.c
@@ -2,43 +2,43 @@
 #include <stdlib.h>
 #include <math.h>
 
-float x = 2.28; //<--ЗНАЧЕНИЕ ХЭ ПИСАТЬ СЮДА)00)))00))
-float beg = 0; //начало интервала
-float end = 4; //конец
-float intrvl = 1; //длина интервала
+double x = 2.28; //<--ЗНАЧЕНИЕ ХЭ ПИСАТЬ СЮДА)00)))00))
+double beg = 0; //начало интервала
+double end = 4; //конец
+double intrvl = 1; //длина интервала
 int cnt = 3; //кол-во переменных для лагранжа
 
-float f(float x)
+double f(double x)
 {
     return x * x + 2;
 }
 
-void output(float tmp, int i, float *x_val, float *y_val)
+void output(double tmp, int i, double *x_val, double *y_val)
 {
     for (int k = 0; k < cnt; k++) {
         if (k == i)
             continue;
-        printf("(%.1f - %.1f)", x, x_val[k]);
+        printf("(%.3lf - %.3lf)", x, x_val[k]);
     }
     printf("\n");
     for (int k = 0; k < cnt - 1; k++)
-        printf("-----------");
-    printf(" * %.1f = %.1f\n", y_val[i], tmp * y_val[i]);
+        printf("---------------");
+    printf(" * %.3lf = %.3lf\n", y_val[i], tmp * y_val[i]);
     for (int k = 0; k < cnt; k++) {
         if (k == i)
             continue;
-        printf("(%.1f - %.1f)", x_val[i], x_val[k]);
+        printf("(%.3lf - %.3lf)", x_val[i], x_val[k]);
     }
     printf("\n");
     printf("\n");
     return;
 }
 
-float langrage(float *x_val, float *y_val)
+double langrage(double *x_val, double *y_val)
 {
-    float res = 0;
+    double res = 0;
     for (int i = 0; i < cnt; i++) {
-        float tmp = 1;
+        double tmp = 1;
         for (int j = 0; j < cnt; j++) {
             if (i == j)
                 continue;
@@ -52,18 +52,18 @@ float langrage(float *x_val, float *y_val)
 
 int main()
 {
-    int point_cnt = floor(fabsf(end - beg) / intrvl) + 1;
-    float *x_val = NULL, *y_val = NULL;
-    x_val = (float*) malloc(sizeof(float) * point_cnt);
-    y_val = (float*) malloc(sizeof(float) * point_cnt);
-    float k = beg;
+    int point_cnt = floor(fabs(end - beg) / intrvl) + 1;
+    double *x_val = NULL, *y_val = NULL;
+    x_val = (double*) malloc(sizeof(double) * point_cnt);
+    y_val = (double*) malloc(sizeof(double) * point_cnt);
+    double k = beg;
     for (int i = 0; i < point_cnt; i++) {
         x_val[i] = k;
         k += intrvl;
         y_val[i] = f(x_val[i]);
     }
-    float answ = langrage(x_val, y_val);
-    printf("%.1f ", answ);
+    double answ = langrage(x_val, y_val);
+    printf("%.3lf ", answ);
     printf("\n");
     return 0;
 }
