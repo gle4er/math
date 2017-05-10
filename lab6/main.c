774820b405d10979ce5252bbbb921ac0d27fb6c5
Гриша Тарасенко <fle4er@yandex.ru>
2017-05-10 10:44:39 +0700

    Рабочий вариант эйткена (буллитпруф)

--- lab6/main.c
+++ lab6/main.c
@@ -2,16 +2,19 @@
 #include <math.h>
 #include <stdlib.h>
 
-double beg = 0; //начало интервала
-double end = 6; //конец
-double intrvl = 0.5; //длина интервала
+double beg = 0.6; //начало интервала
+double end = 1.21; //конец
+double intrvl = 0.2; //длина интервала
+
+double *x_val = NULL;
+double *y_val = NULL;
 
 double f(double x)
 {
-    return sqrt(x);
+    return cbrt(x);
 }
 
-void tabl(double *x_val, double *y_val, int point_cnt)
+void tabl(int point_cnt)
 {
     printf("x\t");
     for (int i = 0; i < point_cnt; i++)
@@ -21,26 +24,19 @@ void tabl(double *x_val, double *y_val, int point_cnt)
         printf("%.2lf\t", y_val[i]);
 }
 
-int eitken(int pnt_cnt, double *xs, double *y_val, double x)
+double eitken(int n, int m, double x)
 {
-    double ys[pnt_cnt];
-    for (int i = 0; i < pnt_cnt; i++)
-        ys[i] = y_val[i];
-    for (int i = 1; i < pnt_cnt; i++) {
-        for (int j = 0, cnt = 0; (i + j) < pnt_cnt; j += i, cnt++) {
-            double tmp = ys[j] * (x - xs[i + j]) - ys[j + 1] * (x - xs[j]);
-            tmp /= (xs[j] - xs[i + j]);
-            ys[cnt] = tmp;
-        }
-    }
-    printf("P(x) = %.2f\t", ys[0]);
+    if (m == n)
+        return y_val[n];
+    return ((x - x_val[n]) * eitken(n + 1, m, x) - 
+            (x - x_val[m]) * eitken(n, m - 1, x)) /
+            (x_val[m] - x_val[n]);
     return 0;
 }
 
 int main()
 {
     int point_cnt = floor(fabs(end - beg) / intrvl) + 1;
-    double *x_val = NULL, *y_val = NULL;
     x_val = (double*) malloc(sizeof(double) * point_cnt);
     y_val = (double*) malloc(sizeof(double) * point_cnt);
     double k = beg;
@@ -49,14 +45,14 @@ int main()
         k += intrvl;
         y_val[i] = f(x_val[i]);
     }
-    tabl(x_val, y_val, point_cnt);
+    tabl(point_cnt);
     printf("\n");
     printf("%d\n", point_cnt);
-    for (int i = 0; i < point_cnt; i++) {
+    for (int i = 0; i < point_cnt * 2 - 1; i++) {
         double x = beg + intrvl / 2 * i;
         printf("x = %.2lf\t", x);
-        eitken(point_cnt, x_val, y_val, x);
-        printf("f(x) = %.2lf\n", f(x));
+        double answ = eitken(0, point_cnt - 1, x);
+        printf("p(x) = %lf\t f(x) = %lf\n", answ, f(x));
     }
     return 0;
 }
